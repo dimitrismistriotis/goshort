b310fdfca56470c1a74898c47dafc1321c371c57
Dimitrios Mistriotis <dimitrismistriotis@gmail.com>
2014-09-21 14:05:48 +0100

    Debug to info

--- server/setup_server.go
+++ server/setup_server.go
@@ -11,10 +11,10 @@ import (
 // Gets Redis instance from configuration parameters or environment's REDISTOGO_URL.
 func redis_instance(log *logging.Logger) (redis_con redis.Conn, err error) {
 	if os.Getenv("REDISTOGO_URL") != "" {
-		log.Debug("Instantiating with REDISTOGO_URL.")
+		log.Info("Instantiating with REDISTOGO_URL.")
 		redis_con, err = redisurl.ConnectToURL(os.Getenv("REDISTOGO_URL"))
 	} else {
-		log.Debug("Instantiating with local-host.")
+		log.Info("Instantiating with local-host.")
 		redis_con, err = redis.Dial("tcp", ":6379")
 	}
 
